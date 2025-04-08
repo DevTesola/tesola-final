aded39c4b29b4a18a29e7947c5f897a99092b112
DevTesola <Devtesola@gmail.com>
2025-04-09 00:43:28 +0900

    switch to JS to bypass TypeScript issue

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{
+      color: "#fff",
+      background: "#000",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <h1>Tesola Dashboard 시작됨 🎉</h1>
+    </div>
+  );
+}
